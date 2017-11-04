69f2a30fb5f083f41a9663725fb98e016b80cbfb
tim <tim.parkerd@gmail.com>
2017-11-03 21:40:40 -0400

    Added placeholder pages for registration, login, and task creation/listing

---
+++ client/javascripts/app.js
@@ -0,0 +1,47 @@
+// This is the actual application that contains the module, there will be
+// other files for controllers and whatnot, I think...
+(() => {
+  'use strict'
+  let dependencies = ['ngRoute', 'ngResource']
+  angular.module('app', dependencies)
+    .config(($routeProvider, $locationProvider) => {
+      $locationProvider.html5Mode(true)
+      $routeProvider
+        .when('/', {
+          templateUrl: '/partials/home',
+          controller: 'mainCtrl'
+        })
+        .when('/register', {
+          templateUrl: 'partials/register',
+          controller: 'registerCtrl'
+        })
+        .when('/login', {
+          templateUrl: 'partials/login',
+          controller: 'loginCtrl'
+        })
+        .when('/tasks/', {
+          templateUrl: 'partials/tasks',
+          controller: 'tasksCtrl'
+        })
+        .when('/task/create', {
+          templateUrl: 'partials/task-create',
+          controller: 'taskCreateCtrl'
+        })
+        // TODO(timp): To be implemented once a connection can be made to the
+        // database and there is existing users
+        .when('/user/:id', {
+          templateUrl: 'partials/user-profile',
+          controller: 'userProfileCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        })
+    })
+    .controller('mainCtrl', main)
+    .controller('registerCtrl', registerCtrl)
+    .controller('loginCtrl', loginCtrl)
+    .controller('tasksCtrl', taskCtrls.list)
+    .controller('taskCreateCtrl', taskCtrls.create)
+    .controller('userProfileCtrl', userCtrls.viewProfile)
+
+})()
