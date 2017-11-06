421f0c9e5052c9da4aa2dd458c90942fb4d290d3
tim <tim.parkerd@gmail.com>
2017-11-06 01:16:18 -0500

    stubbed out the components of angular app

--- server/public/js/controllers.js
+++ server/public/js/controllers.js
@@ -1,24 +1,27 @@
 function main($scope, $http, $routeParams) {
   console.log('Loaded main controller')
 }
-function registerCtrl($scope, $http, $routeParams) {
-  console.log('Loaded registration controller')
-}
-function loginCtrl($scope, $http, $routeParams) {
-  console.log('Loaded login controller')
+
+let userCtrls = {
+  register: function($scope, $http, $routeParams) {
+    console.log('Loaded registration controller')
+  },
+
+  login: function($scope, $http, $routeParams) {
+    console.log('Loaded login controller')
+  },
+
+  viewProfile: function($scope, $http, $routeParams) {
+    console.log('Loaded user profile controller.')
+  }
 }
 
 let taskCtrls = {
   list: function($scope, $http, $routeParams) {
     console.log('Loaded task listing controller.')
   },
+  
   create: function($scope, $http, $routeParams) {
     console.log('Loaded task creation controller.')
-  }
-}
-
-let userCtrls = {
-  viewProfile: function($scope, $http, $routeParams) {
-    console.log('Loaded user profile controller.')
   }
 }
