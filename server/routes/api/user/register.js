93177362b9c99eb904e7b5b3ed41c446542248fb
Tim P <Tim.ParkerD@gmail.com>
2017-11-06 21:12:43 -0500

    connection to db for registration endpoint

--- server/routes/api/user/register.js
+++ server/routes/api/user/register.js
@@ -1,10 +1,33 @@
 'use strict'
 const router   = require('express').Router(),
-      passport = require('passport')
+      passport = require('passport'),
+      mongoose = require('mongoose')
 
 // Registration
 router.post('/', (req, res) => {
   'use strict'
+  var url = 'mongodb://localhost/employeetracker'
+
+  // mongoose.connect(url, {useMongoClient: true}, function(err, db) {
+  //   if (err) return
+  //
+  //   var collection = db.collection('employee')
+  //   collection.insert({firstName: 'John', lastName: 'Doe', username: 'jdoe', password: 'admin'}, function(err, result) {
+  //     collection.find({name: 'John'}).toArray(function(err, docs) {
+  //       console.log(docs[0])
+  //       db.close()
+  //     })
+  //   })
+  // })
+
+  mongoose.connect(url, {
+    useMongoClient: true
+  })
+
+  let db = mongoose.connection
+
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 
   // TODO
 
