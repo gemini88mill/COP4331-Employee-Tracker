c1ebc22112016d0cf999d9fd2269bcb02fc93ff7
Raphael <gemini88mill@gmail.com>
2017-11-08 21:21:24 -0500

    camera worksish

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/photoActivity.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/photoActivity.java
@@ -5,22 +5,37 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import static com.example.raphael.bigbrother.R.id.image;
+import static com.example.raphael.bigbrother.R.id.pictureCapture;
 
 public class photoActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    private ImageView imageView;
+    private ImageView mimageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+
+        Button photoButton = (Button) this.findViewById(R.id.pictureCapture);
+        photoButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(cameraIntent.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
     }
 
-    private void dispatchTakePictureIntent(){
+    private void dispatchTakePictureIntent(View v){
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(takePictureIntent.resolveActivity(getPackageManager()) != null){
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -32,7 +47,7 @@ public class photoActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+            mimageView.setImageBitmap(imageBitmap);
         }
     }
 }
