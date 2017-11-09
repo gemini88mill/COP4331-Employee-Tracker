546c039fd646549cb9019accd26ea91303f03142
Raphael <gemini88mill@gmail.com>
2017-11-09 16:46:06 -0500

    added home activity buttons

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/HomeActivity.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.raphael.bigbrother;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onTaskListClick(View view){
+
+    }
+
+    public void onMapViewClick(View view){
+
+    }
+
+    public void onClockInOutClick(View view){
+
+    }
+
+    public void onSignOutClick(View view){
+        
     }
 }
