abf8bc152f5e02339e2b224f181a90a72bb9011d
Raphael <gemini88mill@gmail.com>
2017-11-09 16:57:50 -0500

    added mapView

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/HomeActivity.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.raphael.bigbrother;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,18 +14,27 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void onTaskListClick(View view){
+        System.out.println("to taskList");
 
+        Intent intent = new Intent(this, TasksActivity.class);
+        startActivity(intent);
     }
 
     public void onMapViewClick(View view){
+        System.out.println("to mapView");
 
+        Intent intent = new Intent(this, LocationActivity.class);
+        startActivity(intent);
     }
 
     public void onClockInOutClick(View view){
-
+        //todo send JSON to clock in and out
     }
 
     public void onSignOutClick(View view){
-        
+        System.out.println("back to start");
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
