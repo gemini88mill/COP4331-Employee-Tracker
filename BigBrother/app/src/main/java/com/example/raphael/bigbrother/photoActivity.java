f8498668f039165f84d1ff4b6dd52db76d408e95
Raphael <gemini88mill@gmail.com>
2017-11-08 20:46:25 -0500

    added new method for picture capture

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/photoActivity.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/photoActivity.java
@@ -1,9 +1,13 @@
 package com.example.raphael.bigbrother;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class photoActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +16,9 @@ public class photoActivity extends AppCompatActivity {
     }
 
     private void dispatchTakePictureIntent(){
-        
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
