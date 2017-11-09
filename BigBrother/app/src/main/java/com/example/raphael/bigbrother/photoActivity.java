6d06a6552e40c6215589af7c8e7b3db02d29ad84
Raphael <gemini88mill@gmail.com>
2017-11-08 20:38:39 -0500

    working on connections to the activities

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/photoActivity.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/photoActivity.java
@@ -10,4 +10,8 @@ public class photoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
     }
+
+    private void dispatchTakePictureIntent(){
+        
+    }
 }
