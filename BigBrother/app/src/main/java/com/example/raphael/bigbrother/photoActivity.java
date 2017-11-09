63698ec5cf3087e920d4e3d3ed87737143a2ec17
Raphael <gemini88mill@gmail.com>
2017-11-08 21:00:11 -0500

    added method for bit map display

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/photoActivity.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/photoActivity.java
@@ -1,13 +1,18 @@
 package com.example.raphael.bigbrother;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import static com.example.raphael.bigbrother.R.id.image;
 
 public class photoActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,15 @@ public class photoActivity extends AppCompatActivity {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(takePictureIntent.resolveActivity(getPackageManager()) != null){
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
         }
     }
 }
