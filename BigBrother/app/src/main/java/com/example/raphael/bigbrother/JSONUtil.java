5b4e60b84ff08b5fcb87dc9101e6e90f2142814f
Raphael <gemini88mill@gmail.com>
2017-11-10 09:38:55 -0500

    quarantined classes

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/JSONUtil.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/JSONUtil.java
@@ -8,9 +8,17 @@ import com.google.gson.*;
 
 public class JSONUtil {
 
+    
+
+//---Quarantine---
     class LoginPackage{
         private String userName;
         private String password;
+
+        public LoginPackage(String userName, String password) {
+            this.userName = userName;
+            this.password = password;
+        }
     }
 
     class SignupPackage{
@@ -19,12 +27,26 @@ public class JSONUtil {
         private String firstName;
         private String lastName;
         private int adminRequestLevel;
+
+        public SignupPackage(String username, String password, String firstName, String lastName, int adminRequestLevel) {
+            this.username = username;
+            this.password = password;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.adminRequestLevel = adminRequestLevel;
+        }
     }
 
     class locationPackage{
         private double latitude;
         private double longitude;
         private int userID;
+
+        public locationPackage(double latitude, double longitude, int userID) {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.userID = userID;
+        }
     }
 
     class taskListPackage{
@@ -32,17 +54,27 @@ public class JSONUtil {
         private double taskLongitude;
         private String taskName;
         private String teskDescription;
+
+        public taskListPackage(double taskLatitude, double taskLongitude, String taskName, String teskDescription) {
+            this.taskLatitude = taskLatitude;
+            this.taskLongitude = taskLongitude;
+            this.taskName = taskName;
+            this.teskDescription = teskDescription;
+        }
     }
+//---Quarantine---
 
     public void buildJSON(){
         Gson gson = new Gson();
-    }
 
-    public void collectJSON(){
 
     }
 
-    public void receiveJSON(){
+    public void createJson(){
+        Gson gson = new Gson();
+    }
+
+    public void debugJson(){
 
     }
 }
