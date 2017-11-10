a476f5ead1db21fcaa02124415a1ee6fe55964d8
Raphael <gemini88mill@gmail.com>
2017-11-10 10:09:23 -0500

    deleted quarentine

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/JSONUtil.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/JSONUtil.java
@@ -80,60 +80,6 @@ public class JSONUtil {
         this.taskDescription = taskDescription;
     }
 
-    ////---Quarantine---
-//    class LoginPackage{
-//        private String userName;
-//        private String password;
-//
-//        public LoginPackage(String userName, String password) {
-//            this.userName = userName;
-//            this.password = password;
-//        }
-//    }
-//
-//    class SignupPackage{
-//        private String username;
-//        private String password;
-//        private String firstName;
-//        private String lastName;
-//        private int adminRequestLevel;
-//
-//        public SignupPackage(String username, String password, String firstName, String lastName, int adminRequestLevel) {
-//            this.username = username;
-//            this.password = password;
-//            this.firstName = firstName;
-//            this.lastName = lastName;
-//            this.adminRequestLevel = adminRequestLevel;
-//        }
-//    }
-//
-//    class locationPackage{
-//        private double latitude;
-//        private double longitude;
-//        private int userID;
-//
-//        public locationPackage(double latitude, double longitude, int userID) {
-//            this.latitude = latitude;
-//            this.longitude = longitude;
-//            this.userID = userID;
-//        }
-//    }
-//
-//    class taskListPackage{
-//        private double taskLatitude;
-//        private double taskLongitude;
-//        private String taskName;
-//        private String teskDescription;
-//
-//        public taskListPackage(double taskLatitude, double taskLongitude, String taskName, String teskDescription) {
-//            this.taskLatitude = taskLatitude;
-//            this.taskLongitude = taskLongitude;
-//            this.taskName = taskName;
-//            this.teskDescription = teskDescription;
-//        }
-//    }
-////---Quarantine---
-
     public void buildJSON(JSONUtil info){
         Gson gson = new Gson();
         gson.toJson(info);
@@ -145,7 +91,7 @@ public class JSONUtil {
 
     public void receiveJSON(Gson gson, String type){
         //receive from server
-        
+
     }
 
     public void debugJson(){
