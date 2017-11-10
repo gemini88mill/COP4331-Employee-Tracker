7533672c9630bb98477e5f6f831c9dd620ace481
Raphael <gemini88mill@gmail.com>
2017-11-09 22:04:11 -0500

    added JSON packages

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/JSONUtil.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/JSONUtil.java
@@ -4,7 +4,39 @@ package com.example.raphael.bigbrother;
  * Created by raphael on 11/6/17.
  */
 
+import com.google.gson.*;
+
 public class JSONUtil {
+
+    class LoginPackage{
+        private String userName;
+        private String password;
+    }
+
+    class SignupPackage{
+        private String username;
+        private String password;
+        private String firstName;
+        private String lastName;
+        private int adminRequestLevel;
+    }
+
+    class locationPackage{
+        private double latitude;
+        private double longitude;
+        private int userID;
+    }
+
+    class taskListPackage{
+        private double taskLatitude;
+        private double taskLongitude;
+        private String taskName;
+        private String teskDescription;
+    }
+
+    public void buildJSON(){
+        Gson gson = new Gson();
+    }
 
     public void collectJSON(){
 
