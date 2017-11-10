09d0cd47c9fdf1515e9528c3a103d98edaba9ed7
Raphael <gemini88mill@gmail.com>
2017-11-10 09:51:42 -0500

    added constructors

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/JSONUtil.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/JSONUtil.java
@@ -6,72 +6,120 @@ package com.example.raphael.bigbrother;
 
 import com.google.gson.*;
 
+import java.util.List;
+
 public class JSONUtil {
 
-    
-
-//---Quarantine---
-    class LoginPackage{
-        private String userName;
-        private String password;
-
-        public LoginPackage(String userName, String password) {
-            this.userName = userName;
-            this.password = password;
-        }
-    }
-
-    class SignupPackage{
-        private String username;
-        private String password;
-        private String firstName;
-        private String lastName;
-        private int adminRequestLevel;
-
-        public SignupPackage(String username, String password, String firstName, String lastName, int adminRequestLevel) {
-            this.username = username;
-            this.password = password;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.adminRequestLevel = adminRequestLevel;
-        }
-    }
-
-    class locationPackage{
+    private class Coordinates{
         private double latitude;
         private double longitude;
-        private int userID;
 
-        public locationPackage(double latitude, double longitude, int userID) {
+        public Coordinates(double latitude, double longitude) {
             this.latitude = latitude;
             this.longitude = longitude;
-            this.userID = userID;
         }
     }
 
-    class taskListPackage{
-        private double taskLatitude;
-        private double taskLongitude;
-        private String taskName;
-        private String teskDescription;
+    private String username;
+    private String password; //should be hashed***
+    private String userFirstName;
+    private String userLastName;
+    private String userPosition;
+    private int userPermissionLevel;
+    private Coordinates userLocation;
+    private List<Coordinates> userTaskLocation;
+    private String taskName;
+    private String taskDescription;
 
-        public taskListPackage(double taskLatitude, double taskLongitude, String taskName, String teskDescription) {
-            this.taskLatitude = taskLatitude;
-            this.taskLongitude = taskLongitude;
-            this.taskName = taskName;
-            this.teskDescription = teskDescription;
-        }
+    public JSONUtil(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
-//---Quarantine---
 
-    public void buildJSON(){
+    public JSONUtil(String username, String password, String userFirstName, String userLastName) {
+        this.username = username;
+        this.password = password;
+        this.userFirstName = userFirstName;
+        this.userLastName = userLastName;
+    }
+
+    public JSONUtil(String username, Coordinates userLocation) {
+        this.username = username;
+        this.userLocation = userLocation;
+    }
+
+    public JSONUtil(List<Coordinates> userTaskLocation, String taskName, String taskDescription) {
+        this.userTaskLocation = userTaskLocation;
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+    }
+
+    ////---Quarantine---
+//    class LoginPackage{
+//        private String userName;
+//        private String password;
+//
+//        public LoginPackage(String userName, String password) {
+//            this.userName = userName;
+//            this.password = password;
+//        }
+//    }
+//
+//    class SignupPackage{
+//        private String username;
+//        private String password;
+//        private String firstName;
+//        private String lastName;
+//        private int adminRequestLevel;
+//
+//        public SignupPackage(String username, String password, String firstName, String lastName, int adminRequestLevel) {
+//            this.username = username;
+//            this.password = password;
+//            this.firstName = firstName;
+//            this.lastName = lastName;
+//            this.adminRequestLevel = adminRequestLevel;
+//        }
+//    }
+//
+//    class locationPackage{
+//        private double latitude;
+//        private double longitude;
+//        private int userID;
+//
+//        public locationPackage(double latitude, double longitude, int userID) {
+//            this.latitude = latitude;
+//            this.longitude = longitude;
+//            this.userID = userID;
+//        }
+//    }
+//
+//    class taskListPackage{
+//        private double taskLatitude;
+//        private double taskLongitude;
+//        private String taskName;
+//        private String teskDescription;
+//
+//        public taskListPackage(double taskLatitude, double taskLongitude, String taskName, String teskDescription) {
+//            this.taskLatitude = taskLatitude;
+//            this.taskLongitude = taskLongitude;
+//            this.taskName = taskName;
+//            this.teskDescription = teskDescription;
+//        }
+//    }
+////---Quarantine---
+
+    public void buildJSON(JSONUtil info){
         Gson gson = new Gson();
 
 
     }
 
-    public void createJson(){
-        Gson gson = new Gson();
+    public void sendJSON(){
+
+    }
+
+    public void receiveJSON(){
+        
     }
 
     public void debugJson(){
