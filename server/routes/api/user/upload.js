b7639c9163032e5dbbc381ff6e67de77bb4e769d
tim <tim.parkerd@gmail.com>
2017-11-10 22:53:22 -0500

    Clean up upload

--- server/routes/api/user/upload.js
+++ server/routes/api/user/upload.js
@@ -6,9 +6,20 @@ const router     = require('express').Router(),
       multer     = require('multer'),
       fs         = require('fs')
 
-
 router.post('/', (req, res) => {
   'use strict'
+  let imageFilter = function (req, file, cb) {
+    // accept image only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+      return cb(new Error('Only image files are allowed!'), false)
+    }
+    cb(null, true)
+  }
+
+  let cleanFolder = function (folderPath) {
+    // delete files inside folder but not the folder itself
+    del.sync([`${folderPath}/**`, `!${folderPath}`])
+  }
 
   // TODO(timp): replace the final string in the directory with the
   // unique username of the user so that the name of the image can be
@@ -23,11 +34,8 @@ router.post('/', (req, res) => {
     },
     filename: function(req, file, cb) {
       cb(null, file.originalname + '')
-      console.log(file);
     }
   })
-
-  console.log(storage);
 
   let upload = multer({
     dest: dir,
@@ -43,104 +51,5 @@ router.post('/', (req, res) => {
     res.status(200).json({ message: 'File was uploaded successfully.' })
     // Everything went fine
   })
-
-
 })
-
-var imageFilter = function (req, file, cb) {
-    // accept image only
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-        return cb(new Error('Only image files are allowed!'), false)
-    }
-    cb(null, true)
-}
-
-var cleanFolder = function (folderPath) {
-    // delete files inside folder but not the folder itself
-    del.sync([`${folderPath}/**`, `!${folderPath}`])
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Upload image
-// router.post('/', (req, res) => {
-//   'use strict'
-//
-//   // Stub of the uploading functionality
-//   // NOTE(timp): this is non-functional in its current state
-//   if (!req.files)
-//     return res.status(400).json({ message: 'Image file was not found.' })
-//
-//   // Fetch the uploaded file
-//   let sampleFile = req.files.sampleFile
-//
-//   // TODO(timp): Generate unique filename or directory (perhaps all images)
-//   //             are stored within a unique directory for each user.
-//   let fileName = path.join('..', '..', 'public', 'uploads', 'thing.png')
-//   sampleFile.mv(fileName, function(err) {
-//     if (err)
-//       return res.status(500).json({ message: err })
-//     res.status(200).send({ message: 'Upload successful!' })
-//   })
-// })
-//
-// router.post('/', function(req, res) {
-//   console.log(req.files);
-//
-//   if (!req.files)
-//     return res.status(400).send('No files were uploaded.');
-//
-//   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-//   let sampleFile = req.files.sampleFile;
-//   console.log(sampleFile);
-//
-//   // Use the mv() method to place the file somewhere on your server
-//   let fileName = path.join('..', '..', 'public', 'uploads', 'thing.png')
-//   sampleFile.mv(fileName, function(err) {
-//     if (err)
-//       return res.status(500).send(err);
-//
-//     res.send('File uploaded!');
-//   });
-// });
-
-// route.post('/', (req, res, next) => {
-//   var fstream
-//   req.pip(req.busboy)
-//   req.busboy.on('file', (fieldname, file, filename) => {
-//     console.log('Uploading: ' + filename)
-//
-//     // Path where image will be uploaded
-//     fstream = fs.createWriteStream(path.join('..', '..', 'public', 'uploads', filename))
-//     file.pipe(fstream)
-//     fstream.on('clone', () => {
-//       console.log('Upload finished of ' + filename)
-//       res.redirect('back')
-//     })
-//   })
-// })
-//
-
-
 module.exports = router
