04f3c7db5a6a41be9e9507f936e33ad91fe62f8d
tim <tim.parkerd@gmail.com>
2017-11-11 01:00:46 -0500

    Simplified api code for getting information on user

--- server/routes/api/user/list.js
+++ server/routes/api/user/list.js
@@ -5,28 +5,11 @@ const router   = require('express').Router(),
 
 router.post('/', (req, res) => {
   'use strict'
-  // Determine what kind of request
-  // 1. All users
-  // 2. One user
-  // 3. Many users
-  // console.log(req.body);
-  // If there were no user information
-  if (!req.body) {
-    Employee.find((err, employees) => {
-      if (err)
-      res.send(err)
-      else
-      return res.json(employees)
-    })
-  // Otherwise, find users
-  } else {
-    Employee.find( { username: { $in: req.body.users } } , (err, employees) => {
-      if (err)
-      res.send(err)
-      else
-      return res.json(employees)
-    })
-  }
+  Employee.find( { username: { $in: req.body } } , (err, user) => {
+    if (err) return res.status(500).json(err)
+    else if (user.length <= 0) return res.status(404).json(user)
+    else return res.json(user)
+  })
 })
 
 module.exports = router
