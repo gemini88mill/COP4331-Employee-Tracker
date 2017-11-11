e59d71253ed3c5dee00721b25ba15343384d5ce8
Tim Parker <tparkerd@users.noreply.github.com>
2017-11-10 23:04:25 -0500

    Google Maps & Employee Profile Update (#4)
    * Profile photo loads onto google maps. To be implemented: custom pin via canvas
    
    * placeholder code for google maps integration

--- server/public/js/controllers.js
+++ server/public/js/controllers.js
@@ -34,23 +34,93 @@ let userCtrls = {
 
   view: function($scope, $http, $routeParams) {
     $scope.id = $routeParams.id
+    $scope.username = 'lknope'
+    $scope.firstName = 'Leslie'
+    $scope.lastName = 'Knope'
+    $scope.position = 'Employee'
+    $scope.teams = ['Team 1', 'Team Ann Rocks']
     console.log('Loaded user profile controller.')
 
+    // Create a canvas
+    // Reference: https://codepen.io/kelvinw88/pen/myKWqQ
+    let imageObj = new Image,
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d'),
+        src = '../img/uploads/lknope.png'
+
+    const pinColor = '#9132FB'
+
+    canvas.setAttribute('height', 57)
+    canvas.setAttribute('width', 46)
+    imageObj.src = src
+
+    imageObj.onload = () => {
+      // Draw the border
+      // <!-- Purcple Circle (marker) -->
+      context.beginPath()
+      context.arc(23, 23, 22, 0, 2 * Math.PI)
+      context.lineWidth = 1
+      context.strokeStyle = pinColor
+      context.stroke()
+      context.fillStyle = pinColor
+      context.fill()
 
 
-    var cities = "Orlando, USA";
+      // <!-- Bottom part of Marker  -->
+      context.beginPath();
+      context.lineWidth = 4
+      context.moveTo(7, 36)
+      context.lineTo(23, 55)
+      context.lineTo(39, 36)
+      context.lineWidth = 4
+      context.lineJoin = 'round'
+      context.strokeStyle = pinColor
+      context.stroke()
+      context.fillStyle = pinColor
+      context.fill()
+      context.closePath()
+
+    	// <!-- White Circle -->
+      context.beginPath()
+      context.arc(23, 23, 18, 0, 2 * Math.PI)
+      context.fillStyle = 'white'
+      context.fill()
+
+
+    	// <!-- Display Picture -->
+      context.save();
+      context.beginPath()
+      context.arc(23, 23, 16, 0, Math.PI * 2, true)
+      context.closePath()
+      context.clip()
+
+      // <!-- Place Image -->
+      context.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height, 0, 0, 50, 50)
+      context.lineWidth = 1
+      context.strokeStyle = 'white'
+      context.stroke()
+      context.restore()
+    }
+      imageObj.src = src
+
+    var city = "Orlando, USA";
     var geocoder= new google.maps.Geocoder();
 
      $scope.markers = [];
 
-     var createMarker = function (info){
-          var marker = new google.maps.Marker({
-              map: $scope.map,
-              position: new google.maps.LatLng(info.lat(), info.lng())
-          });
-     }
+    var createMarker = function (info) {
+      let image = '../img/uploads/lknope.png'
+      let icon = {
+        url: canvas.toDataURL()
+      }
+      var marker = new google.maps.Marker({
+        map: $scope.map,
+        icon: icon,
+        position: new google.maps.LatLng(info.lat(), info.lng())
+        });
+    }
 
-     geocoder.geocode( { 'address': cities }, function(results, status) {
+     geocoder.geocode( { 'address': city }, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
           newAddress = results[0].geometry.location;
           $scope.map.setCenter(newAddress);
@@ -59,19 +129,11 @@ let userCtrls = {
      });
 
     $scope.mapOptions = {
-          zoom: 4,
-          //center: new google.maps.LatLng(41.923, 12.513),
-          mapTypeId: google.maps.MapTypeId.TERRAIN
+          zoom: 6,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
       }
 
       $scope.map = new google.maps.Map(document.getElementById('map'), $scope.mapOptions);
-
-
-      //issue of google maps inside bootstap model issue
-      $('#myModal').on('shown.bs.modal', function(){
-      google.maps.event.trigger(map, 'resize');
-     $scope.map.setCenter(new google.maps.LatLng(newAddress.lat(), newAddress.lng()));
-    })
   }
 }
 
