e8b2c54e746d2b7eeed7aef90522702981d18851
tim <tim.parkerd@gmail.com>
2017-11-10 22:45:39 -0500

    placeholder code for google maps integration

--- server/public/js/controllers.js
+++ server/public/js/controllers.js
@@ -13,26 +13,84 @@ let userCtrls = {
 
   view: function($scope, $http, $routeParams) {
     $scope.id = $routeParams.id
+    $scope.username = 'lknope'
+    $scope.firstName = 'Leslie'
+    $scope.lastName = 'Knope'
+    $scope.position = 'Employee'
+    $scope.teams = ['Team 1', 'Team Ann Rocks']
     console.log('Loaded user profile controller.')
 
+    // Create a canvas
+    // Reference: https://codepen.io/kelvinw88/pen/myKWqQ
+    let imageObj = new Image,
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d'),
+        src = '../img/uploads/lknope.png'
+
+    const pinColor = '#9132FB'
+
+    canvas.setAttribute('height', 57)
+    canvas.setAttribute('width', 46)
+    imageObj.src = src
+
+    imageObj.onload = () => {
+      // Draw the border
+      // <!-- Purcple Circle (marker) -->
+      context.beginPath()
+      context.arc(23, 23, 22, 0, 2 * Math.PI)
+      context.lineWidth = 1
+      context.strokeStyle = pinColor
+      context.stroke()
+      context.fillStyle = pinColor
+      context.fill()
 
 
-    var cities = "Indiana, USA";
+      // <!-- Bottom part of Marker  -->
+      context.beginPath();
+      context.lineWidth = 4
+      context.moveTo(7, 36)
+      context.lineTo(23, 55)
+      context.lineTo(39, 36)
+      context.lineWidth = 4
+      context.lineJoin = 'round'
+      context.strokeStyle = pinColor
+      context.stroke()
+      context.fillStyle = pinColor
+      context.fill()
+      context.closePath()
+
+    	// <!-- White Circle -->
+      context.beginPath()
+      context.arc(23, 23, 18, 0, 2 * Math.PI)
+      context.fillStyle = 'white'
+      context.fill()
+
+
+    	// <!-- Display Picture -->
+      context.save();
+      context.beginPath()
+      context.arc(23, 23, 16, 0, Math.PI * 2, true)
+      context.closePath()
+      context.clip()
+
+      // <!-- Place Image -->
+      context.drawImage(imageObj, 0, 0, imageObj.width, imageObj.height, 0, 0, 50, 50)
+      context.lineWidth = 1
+      context.strokeStyle = 'white'
+      context.stroke()
+      context.restore()
+    }
+      imageObj.src = src
+
+    var city = "Orlando, USA";
     var geocoder= new google.maps.Geocoder();
 
      $scope.markers = [];
 
-    // Possible use this approach instead
-    // https://codepen.io/dylanvann/pen/yNWdxJ?editors=0110
-    // or
-    // this: https://codepen.io/kelvinw88/pen/myKWqQ !!!
-    var createMarker = function (info){
+    var createMarker = function (info) {
       let image = '../img/uploads/lknope.png'
       let icon = {
-        url: image,
-        scaledSize: new google.maps.Size(100, 100),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(50, 50)
+        url: canvas.toDataURL()
       }
       var marker = new google.maps.Marker({
         map: $scope.map,
@@ -41,7 +99,7 @@ let userCtrls = {
         });
     }
 
-     geocoder.geocode( { 'address': cities }, function(results, status) {
+     geocoder.geocode( { 'address': city }, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
           newAddress = results[0].geometry.location;
           $scope.map.setCenter(newAddress);
@@ -50,19 +108,11 @@ let userCtrls = {
      });
 
     $scope.mapOptions = {
-          zoom: 4,
-          //center: new google.maps.LatLng(41.923, 12.513),
-          mapTypeId: google.maps.MapTypeId.TERRAIN
+          zoom: 6,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
       }
 
       $scope.map = new google.maps.Map(document.getElementById('map'), $scope.mapOptions);
-
-
-      //issue of google maps inside bootstap model issue
-      $('#myModal').on('shown.bs.modal', function(){
-      google.maps.event.trigger(map, 'resize');
-     $scope.map.setCenter(new google.maps.LatLng(newAddress.lat(), newAddress.lng()));
-    })
   }
 }
 
