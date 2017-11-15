29c39915b0ac0f0a801387abc50c566583b1995a
Raphael <gemini88mill@gmail.com>
2017-11-14 20:03:05 -0500

    added collection methods to sendJSONRequest

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/LoginActivity.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,7 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void sendJSONRequest(View view){
-        System.out.println("Sending JSON...");
+        //System.out.println("Sending JSON...");
+
+        //get EditText Values
+        EditText usernameTextField = (EditText) findViewById(R.id.usernameField);
+        EditText passwordTextField = (EditText) findViewById(R.id.passwordField);
+
+        //for debug
+        System.out.println("Values Collected: " + usernameTextField + " " + passwordTextField);
+
 
         Intent intent = new Intent(this, photoActivity.class);
         startActivity(intent);
