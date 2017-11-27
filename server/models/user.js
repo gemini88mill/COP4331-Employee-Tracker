5e139cd03ee7d4d2f539a77c6c4078438c1d98f1
tim <tim.parkerd@gmail.com>
2017-11-27 13:44:29 -0500

    Changed expected values for user location. It had still been expecting an array of lats and lngs, also a typo. Google maps api expects 'lng' instead of 'long'

--- server/models/user.js
+++ server/models/user.js
@@ -13,27 +13,27 @@ function arrayLimit(val) { // location array cannot exceed size 5
 
 // used for base employee, admin, and support; things associated with employee are NOT required in the document
 var userSchema = new mongoose.Schema( {
-    
+
     firstName:  {
                     type:       String,
                     validate:   /^[A-Z]'?[- a-zA-Z]+$/, // name is A-Z, a-z, hyphens and apostrophes
                     required:   true,
                     trim:       true
                 },
-                
+
     lastName:   {
                     type:       String,
                     validate:   /^[A-Z]'?[- a-zA-Z]+$/,
                     required:   true,
                     trim:       true
                 },
-    
+
      // privilege level, employee = 0, admin = 1, support = 2
     privilege:  {
                     type:       Number,
                     default:    0
                 },
-    
+
     // username is a uniqued, required, all lowercase string, spaces are trimmed
     username:   {
                     type:       String,
@@ -42,7 +42,7 @@ var userSchema = new mongoose.Schema( {
                     unique:     true,
                     lowercase:  true
                 },
-                
+
     email:      {
                     type:       String,
                     trim:       true,
@@ -52,9 +52,9 @@ var userSchema = new mongoose.Schema( {
                     validate:   [validateEmail, 'Please fill a valid email address'],
                     match:      [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
                 },
-                
+
     password:   String,
-                
+
     // group: Number, // not sure if we're still doing group assignments for employees
     tasks:      [ // task list stored by task id
                     {
@@ -62,29 +62,29 @@ var userSchema = new mongoose.Schema( {
                         ref: "Task"
                     }
                 ],
-                
+
     picture:    {
                     type:   String, // string for a hosted URL, at least for now
                     default: "https://d3g919u5f14ld1.cloudfront.net/assets/images/users/default-avatar.svg"
                 },
-    
+
     position:   String,
-    
+
     location:   {
                     type: {
-                        lat: [Number],
-                        long: [Number]
+                        lat: Number,
+                        lng: Number
                     },
                 },
-    
+
     timeLastUpdate:     Date, // time of last GPS update
-    
+
     clockStatus: {
-      
+
                     type: Boolean,
                     default: false
                 },
-    
+
 
 });
 
