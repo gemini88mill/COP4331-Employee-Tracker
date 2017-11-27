85cb9ae715eca9ff8755d13f8350cf045e7f5064
Tim Parker <tparkerd@users.noreply.github.com>
2017-11-27 09:12:11 -0500

    Tasks & Clock out (#24)
    * Added clock out functionality
    
    * Pulls tasks from database and can mark tasks completed, not incomplete though
    
    * No longer show the update toast for clock in
    
    * Added clock out route
    
    * Task route functionality for employees implemented
    
    * Removed updates route becaue it was crashing the server since the file does not exist

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/HomeActivity.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/HomeActivity.java
@@ -5,6 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class HomeActivity extends AppCompatActivity {
     public static LocationHandler locationHandler;
 
@@ -38,9 +46,44 @@ public class HomeActivity extends AppCompatActivity {
 
     public void onSignOutClick(View view){
         System.out.println("back to start");
-//        locationHandler.stop(); // TODO(timp): stop listening for changes in position after logging off
+        sendClockOutStatus();
+        locationHandler.locationManager.removeUpdates(locationHandler); // TODO(timp): stop listening for changes in position after logging off
+        locationHandler.locationManager = null;
+        locationHandler = null;
+
 
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void sendClockOutStatus(){
+        String url = getResources().getString(R.string.clockUrl);
+        // pushing to the database
+        JSONObject body = new JSONObject();
+        try {
+            body.put("username", ConnectionHandler.user.username);
+            body.put("clockStatus", "false");
+            body.put("location", LocationHandler.getCoordinates());
+        } catch (JSONException e) {
+
+        }
+
+        // Create a request
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.PUT, url, body, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Successful update location
+                        ConnectionHandler.user.clockStatus = false;
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+
+        // Make the request (add it to the request queue)
+        ConnectionHandler.getInstance(this).addToRequestQueue(jsObjRequest);
     }
 }
