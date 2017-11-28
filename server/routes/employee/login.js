382c71e2d27d40f98d94d5f3b69c71ff4595acf4
tim <tim.parkerd@gmail.com>
2017-11-27 19:03:10 -0500

    Changed the return values based on the error and info messages

--- server/routes/employee/login.js
+++ server/routes/employee/login.js
@@ -6,12 +6,9 @@ var     router      = require('express').Router(),
 // login post route
 router.post('/', function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
-    if (err) return next(err)
-    if (!user) res.status(404)
-    req.logIn(user, function(err) {
-      if (err) return next(err)
-      return res.status(200).json(user)
-    });
+    if (err) return res.status(500).json(err)
+    if (!user) return res.status(404).json(info)
+    return res.status(200).json(user)
   })(req, res, next)
 })
 
