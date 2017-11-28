70997114f1505caef06c887758968be60374ce0a
Raphael <gemini88mill@gmail.com>
2017-11-27 20:17:29 -0500

    not adding color

--- BigBrother/app/src/main/java/com/example/raphael/bigbrother/HomeActivity.java
+++ BigBrother/app/src/main/java/com/example/raphael/bigbrother/HomeActivity.java
@@ -7,9 +7,11 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,6 +30,8 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private String getNameFromDB() {
+
+
         return "Welcome default";
     }
 
